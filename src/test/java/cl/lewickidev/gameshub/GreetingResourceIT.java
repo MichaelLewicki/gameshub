dc1f424401772a15f0726fefe74407ab7e2f5eda
Michael Lewicki <lewickiclass@gmail.com>
2023-11-27 11:40:06 -0300

    general fixes

--- src/test/java/cl/lewickidev/gameshub/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package cl.lewickidev.gameshub;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
